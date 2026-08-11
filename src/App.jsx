085c900cda0418068ac33119349408cd6d9e35b9
Nikilesh <nikileshdasaratha@gmail.com>
2026-08-11 18:33:30 +0530

    files moved

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+    return (<div>
+        <Heading />
+        <Note></Note>
+        <footer>
+          <Footer />
+        </footer>
+    </div>);
+};
+
+export default App;
---
+++ src/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+    return (<header>
+        <h1>
+          This is a Keeper App
+        </h1>
+    </header>);
+}
+
+export default Heading;
